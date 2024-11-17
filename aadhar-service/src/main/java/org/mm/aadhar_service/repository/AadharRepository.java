1594128eec81680696bbd7e56e447647d7703441
ManPatel-07 <patelman7720@gmail.com>
2024-11-17 10:17:14 +0530

    aadhar login done .

--- aadhar-service/src/main/java/org/mm/aadhar_service/repository/AadharRepository.java
+++ aadhar-service/src/main/java/org/mm/aadhar_service/repository/AadharRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface aadharRepository extends JpaRepository<AadharEntity, Long>
+public interface AadharRepository extends JpaRepository<AadharEntity, Long>
 {
 
 }
--- aadhar-service/src/main/java/org/mm/aadhar_service/service/AadharService.java
+++ aadhar-service/src/main/java/org/mm/aadhar_service/service/AadharService.java
@@ -1,9 +1,29 @@
 package org.mm.aadhar_service.service;
 
+import java.util.List;
+
+import org.mm.aadhar_service.entity.AadharEntity;
+import org.mm.aadhar_service.repository.AadharRepository;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class AadharService 
 {
 
+	private final AadharRepository aadharRepository;
+	
+	
+	public AadharEntity createNewAadhar(AadharEntity aadharEntity)
+	{
+		return aadharRepository.save(aadharEntity);
+	}
+	
+	public List<AadharEntity> allAadharsDetails()
+	{
+		return aadharRepository.findAll();
+	}
+	
 }

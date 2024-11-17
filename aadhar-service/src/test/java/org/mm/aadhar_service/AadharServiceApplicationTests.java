9568aa3ff200f2bf1e0cefd8c4e2e8c453c190f2
ManPatel-07 <patelman7720@gmail.com>
2024-11-17 09:54:19 +0530

    create Aadhar-Service

---
+++ aadhar-service/src/test/java/org/mm/aadhar_service/AadharServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mm.aadhar_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadharServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

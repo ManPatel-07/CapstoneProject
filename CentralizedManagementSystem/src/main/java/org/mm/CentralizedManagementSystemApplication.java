cacda243cc09d6c0badea995700b7d03e2618ee0
ManPatel-07 <patelman7720@gmail.com>
2025-03-20 09:54:28 +0530

    Do some changes in backend side
    Do some changes in backend side

---
+++ CentralizedManagementSystem/src/main/java/org/mm/CentralizedManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedManagementSystemApplication.class, args);
+	}
+
+}
